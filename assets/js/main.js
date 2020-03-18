4a3854a9e3df23dce0c6eab4fa16523a165fa613
Ivo Zinenda <izinenda@gmail.com>
2020-03-18 23:54:22 +0200

    admin and header index setup

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+    $('.tooltipped').tooltip();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.modal').modal();
+	$('.materialboxed').materialbox();
+});
